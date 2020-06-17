e2eabf220b6cdc75e25407e2778ef974dc8ef0e2
Sweat <upsidedownplatypus@gmail.com>
2020-06-17 17:16:17 -0400

    Start button deletes itself upon click and timer begins

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function startQuiz() {
-    var button = document.getElementById('startButton');
-    remove.button()
-}
+var button = document.getElementById('startButton');
+var score = 0;
+
+
+function startQuiz(){
+    button.remove(button);  
+    var counter = 50;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("timer");
+        span.textContent = counter;
+      }
+      if (counter === 0) {
+          prompt('Time is up! Your score is ' + score + '. Enter your initials to log your score.');
+          clearInterval(counter);
+      }
+    }, 1000);
+  }
