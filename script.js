b6ed7338b88fd2c4abb148e0b291d4d0786f5fbf
Sweat <upsidedownplatypus@gmail.com>
2020-06-18 23:26:46 -0400

    Cleaned up code

--- script.js
+++ script.js
@@ -1,119 +1,68 @@
 var startButton = document.querySelector('#start-button');
 var timer = document.getElementById('timeleft');
-var quizFrame = document.querySelector('.quiz-frame');
+var quizContent = document.querySelector('.quizcontent');
 var score = document.getElementById('score');
-var questionPrompt = quizFrame.querySelectorAll('h1')[0]
+var questionPrompt = quizContent.querySelectorAll('h1')[0]
 var feedback = document.querySelector('.feedback');
 var pastscores = document.querySelector('#highscore');
 var scoreSubmit = document.querySelector('.scoresubmit')
-
-// Set starting variables
-var questionNumber = 1;
 var time = 90;
+var questionNumber = 1;
 var rightAnswers = 0;
 
-// Create objects with questions
+// Object containing questions, choices, and correct answers
 var qAndAObject = {
     question1: {
-        question : "Choose B",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "B"
+        question : "The scientific name for banana is musa sapientum, which means:",
+        choices : ["Fruit of the wise men", "Long yellow fruit", "One of a bunch", "Bruises easily"],
+        correct : "Fruit of the wise men"
     },
     question2: {
-        question : "Choose A",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "A"
+        question : "Where is the only place in US where bananas are grown commercially?",
+        choices : ["Florida", "Texas", "California", "Hawaii"],
+        correct : "Hawaii"
     },
     question3: {
-        question : "Choose D",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "D"
+        question : "Which of these atrributes about bananas is true?",
+        choices : [ "They can be used to create napalm", "Their peels are poisonus in small amounts", "They are used as currency in some parts of Malaysia", "They float in water"],
+        correct : "They float in water"
     },
     question4: {
-        question : "Choose B",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "B"
+        question : "The proper term for a cluster of bananas is:",
+        choices : ["A hand", "A bunch", "A party", "A tally"],
+        correct : "A hand"
     },
     question5: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "In the video game franchise 'Donkey Kong' where bananas are used as currency, how much is a bunch of bananas worth?",
+        choices : ["5", "10", "25", "100"],
+        correct : "10"
     },
     question6: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "Americans, on average, eat __ pounds of bananas a year.",
+        choices : ["7", "12", "24", "27"],
+        correct : "27"
     },
     question7: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "Thanks to their potassium content, bananas are slightly",
+        choices : ["Psychoactive", "Analgesic", "Radioactive", "Bitter"],
+        correct : "Radioactive"
     },
     question8: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "Gwen Stefani's 2004 hit 'Hollaback Girl' features the proper spelling of 'BANANAS' in the last chorus. Who was the producer of that song?",
+        choices : ["Max Martin","Swizz Beats", "Timbaland", "The Neptunes"],
+        correct : "The Neptunes"
     },
     question9: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "Similar to enjoying something pleasureable or taking narcotics, bananas are the only known fruit to produce this happy chemical in the brain",
+        choices : ["Dopamine", "Serotonin", "Glutamate", "Cortisol"],
+        correct : "Serotonin"
     },
     question10: {
-        question : "Choose C",
-        answers : [
-            "A",
-            "B",
-            "C",
-            "D"
-        ],
-        correct : "C"
+        question : "If you have a headache, you can",
+        choices : ["Eat a banana", "Eat a banana peel", "Rub a banana peel on your forehead", "Deeply inhale it's aroma"],
+        correct : "Rub a banana peel on your forehead"
     },
+    //Replaces the question with the next in line
     getQuestion : function(x){
         var question = "";
         switch(x){
@@ -150,42 +99,44 @@ var qAndAObject = {
         }
         return question;
     },
+    //Replaces the group of choices for the next ones in line
     getAnswers : function(a, b){
-        var answers = "";
+        var choices = "";
         switch(a){
             case 1:
-                answers = this.question1.answers[b];
+                choices = this.question1.choices[b];
                 break;
             case 2:
-                answers = this.question2.answers[b];
+                choices = this.question2.choices[b];
                 break;
             case 3:
-                answers = this.question3.answers[b];
+                choices = this.question3.choices[b];
                 break;
             case 4:
-                answers = this.question4.answers[b];
+                choices = this.question4.choices[b];
                 break;
             case 5:
-                answers = this.question5.answers[b];
+                choices = this.question5.choices[b];
                 break;
             case 6:
-                answers = this.question6.answers[b];
+                choices = this.question6.choices[b];
                 break;
             case 7:
-                answers = this.question7.answers[b];
+                choices = this.question7.choices[b];
                 break;
             case 8:
-                answers = this.question8.answers[b];
+                choices = this.question8.choices[b];
                 break;
             case 9:
-                answers = this.question9.answers[b];
+                choices = this.question9.choices[b];
                 break;
             case 10:
-                answers = this.question10.answers[b];
+                choices = this.question10.choices[b];
                 break;
         }
-        return answers;
+        return choices;
     },
+    //Gets the correct answer for each question
     getCorrectAnswers : function(a){
         var correctAnswers = "";
         switch(a){
@@ -224,10 +175,10 @@ var qAndAObject = {
     }
 };
 
-// Creates object in local storage
-var scoreObj = JSON.parse(localStorage.getItem('scoreObj'));
-if (scoreObj === null){
-    var scoreObj = {
+// Creates object in local storage if there isn't one already
+var scoreobject = JSON.parse(localStorage.getItem('bananaquiz'));
+if (scoreobject === null){
+    var scoreobject = {
         user : [],
         score : []
     }
@@ -255,17 +206,17 @@ function askQuestion(){
     } else{
         // Generates question in an H1
         startButton.style.display = "none";
-        quizFrame.innerHTML = "";
+        quizContent.innerHTML = "";
         questionPrompt = document.createElement('h2');
         questionPrompt.textContent = qAndAObject.getQuestion(questionNumber);
-        quizFrame.append(questionPrompt);
+        quizContent.append(questionPrompt);
         // Generates potential answers within buttons
         for(var i = 0; i < 4; i++){
             answerOption = document.createElement('a');
             answerOption.classList.add('btn', 'col-md-10', 'possibleanswer');
             answerOption.setAttribute("href","#");
             answerOption.textContent = qAndAObject.getAnswers(questionNumber,i);
-            quizFrame.append(answerOption);
+            quizContent.append(answerOption);
         }
         var answerOption = document.querySelector('.possibleanswer')
     }
@@ -280,7 +231,7 @@ function correct(){
 }
 
 // If player answers incorrectly, this runs
-function wrong(){
+function incorrect(){
 
     time -= 10;
     timer.textContent = "Time Left: " + time;
@@ -301,6 +252,7 @@ function endQuiz(){
     finishtext += "<input type='text' id='playername' placeholder='Ooh ooh ah ah'>";
     finishtext += "<input type='submit' value='Submit' class='scoreSubmit'>";
 
+    // Adds a snarky comment if score is under a certain threshold
     var snarkycomment = ""
     if (time >= 30) {
       snarkycomment = "<h1>Harry Belafonte would be proud!</h1>";
@@ -308,28 +260,28 @@ function endQuiz(){
       snarkycomment = "<h1>You don't know bananas.</h1>"
     ]
 
-    quizFrame.innerHTML = snarkycomment + finishtext;
+    quizContent.innerHTML = snarkycomment + finishtext;
 }
 
 function submitScore(){
-    scoreObj.user.push(document.querySelector('#playername').value);
-    scoreObj.score.push(time + rightAnswers);
-    localStorage.setItem('scoreObj', JSON.stringify(scoreObj));
+    scoreobject.user.push(document.querySelector('#playername').value);
+    scoreobject.score.push(time + rightAnswers);
+    localStorage.setItem('bananaquiz', JSON.stringify(scoreobject));
     highScores();
 }
 
 function highScores(){
-    quizFrame.innerHTML = "";
-    var newScoreObj = JSON.parse(localStorage.getItem('scoreObj'))
+    quizContent.innerHTML = "";
+    var newScoreObj = JSON.parse(localStorage.getItem('bananaquiz'))
 
     // Clears quiz page and shows high scores
-    if (scoreObj.user.length !== 0){
+    if (scoreobject.user.length !== 0){
         for(var i = 0; i < newScoreObj.user.length; i++){
             var highscoreString =  newScoreObj.user[i] + " - " + newScoreObj.score[i];
             var highscoreText = document.createElement('p');
             highscoreText.innerHTML = highscoreString;
             highscoreText.classList.add('winnerscreen');
-            quizFrame.prepend(highscoreText);
+            quizContent.prepend(highscoreText);
         }
     }
 
@@ -339,13 +291,13 @@ function highScores(){
     newGame.setAttribute("href","#");
     newGame.classList.add('newgame');
     newGame.classList.add('btn');
-    quizFrame.append(newGame);
+    quizContent.append(newGame);
     var deleteScores = document.createElement('a');
     deleteScores.textContent = "Clear Past Scores";
     deleteScores.setAttribute("href","#");
     deleteScores.classList.add('deletescores');
     deleteScores.classList.add('btn');
-    quizFrame.append(deleteScores);
+    quizContent.append(deleteScores);
 }
 
 pastscores.addEventListener("click",function(){
@@ -359,17 +311,20 @@ startButton.addEventListener("click", function(){
     askQuestion();
 })
 
-// Compares user's response with answer
-quizFrame.addEventListener("click", function(e){
+// This checks for correct answers
+quizContent.addEventListener("click", function(e){
     e.preventDefault();
+    // If the text in the clicked button matches with the correct answer's text, it runs the "correct()" function
     if (e.target.classList.value.indexOf('possibleanswer') !== -1 && e.target.innerHTML === qAndAObject.getCorrectAnswers(questionNumber)){
         correct();
+    // If the text in the clicked button doesn't match with the correct answer's text, it runs the "incorrect()" function
     } else if (e.target.classList.value.indexOf('possibleanswer') > 0){
-        wrong();
+        incorrect();
     }
     
-    // Submit score button click
+    // Clicking the submit score button
     if(e.target.classList.value.indexOf('scoreSubmit') !== -1){
+        // Runs a prompt if nothing is inside the box
         if(document.querySelector('#playername').value.length === 0){
             alert("Hey, buddy, put your name in the box!");
         } else {
@@ -379,9 +334,9 @@ quizFrame.addEventListener("click", function(e){
     
     // Deletes local history on clear score button click
     if(e.target.classList.value.indexOf('deletescores') !== -1){
-        scoreObj.user = [];
-        scoreObj.score = [];
-        localStorage.setItem('scoreObj', JSON.stringify(scoreObj));
+        scoreobject.user = [];
+        scoreobject.score = [];
+        localStorage.setItem('bananaquiz', JSON.stringify(scoreobject));
         highScores();
     }
 
